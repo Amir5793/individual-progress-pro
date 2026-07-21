d1163feea39960e4e9a47fe32129d43ab4cff90d
Amir Ali Nakhostin <amirnakhostin48@gmail.com>
2026-07-21 16:15:58 +0330

    ready for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/individual-progress-pro",
-  // output: "export",
-  basePath: "",
+  basePath: "/individual-progress-pro",
+  output: "export",
+  // basePath: "",
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
